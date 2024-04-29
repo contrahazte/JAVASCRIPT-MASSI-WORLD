9be081fa1dd44e9ef1b10d61094f8dbc3dbb276b
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-04-30 01:13:12 +0200

    Create followsArray.js

---
+++ followsArray.js
@@ -0,0 +1,23 @@
+
+
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+
+function nicknameMap(persons) { 
+    const nickNames = [ ];
+     for (let i = 0; i < persons.length; i++) { 
+    nickNames.push({ follows: persons[i].name + "-" + persons[i].age }); }
+     return nickNames; 
+    } 
+    const followsArray = nicknameMap(persons); console.log(followsArray);
