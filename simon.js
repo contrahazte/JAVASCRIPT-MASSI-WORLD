25a669e53afaebef699d2e2158cff73e99b585bb
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-04-30 01:16:49 +0200

    Create simon.js

---
+++ simon.js
@@ -0,0 +1,10 @@
+ person2.firstName="Simon"
+
+  console.log(person1)
+
+
+Notas generales:
+*ENTONCES: EL VALOR DE firstName EN PERSON1 SERÁ "Simon" PORQUE CUANDO DETERMINO
+*PERSON2=PERSON1 LO QUE CFREO ES UNA REFERENCIA Y CADA VEZ QUE SE MODIFIQUE PERSON2 LO HARÁ PERSON1
+*los objetos se reflejan a través de referencia en nuestro código
+
