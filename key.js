e38806620e6490abac439473f12682ff84753307
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-05-03 16:23:15 +0200

    Create key.js

---
+++ key.js
@@ -0,0 +1,13 @@
+
+let person = {
+    "firstName": "Mario",
+    "lastName": "Rossi",
+    "age": 25
+}
+let entries= Object.entries(person)
+console.log(entries)
+//kEYS-VALUES
+
+let allKeys=Object.keys(person)
+console.log(allKeys)
+//KEYS
