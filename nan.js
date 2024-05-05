b25d57852dba0bd2ab70e38b38e241d6a5afb528
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-05-05 19:49:13 +0200

    Create nan.js

---
+++ nan.js
@@ -0,0 +1,12 @@
+const order = {
+    "customer":{
+    "address":{
+    "city":[],}
+    }}
+    
+
+   
+    
+    if(Object.values(order.customer.address.city===NaN)){
+     console.log("city is required") }
+ 
