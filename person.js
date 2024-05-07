ea6e3105c93815436853f23bbfce45ae32883015
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-05-07 14:06:34 +0200

    Create person.js

---
+++ person.js
@@ -0,0 +1,10 @@
+
+class Person{
+    constructor(firstName,lastName){
+     this.firstName=String(firstName)
+     this.lastName=String(lastName)
+    
+    }
+    }
+let ana= new Person(8,"García")
+console.log(ana)
