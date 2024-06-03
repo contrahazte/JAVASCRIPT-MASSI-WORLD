cfe53ee710ec22d174c2d48c50d968d20e380923
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-06-03 14:38:50 +0200

    Update getInputValue.js

--- getInputValue.js
+++ getInputValue.js
@@ -1,5 +1,25 @@
-function getFirstName(){
-let firstName=document.getElementById("firstName").value;
-return firstName
-}
-getFirstName()
+<html>
+  <body>
+    <div id="container">
+      <form>
+        <div>
+          <label>First Name:</label>
+          <input type="text" class="form-input" id="firstName" value="Mario" />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input type="text" class="form-input" id="lastName" value="Rossi" />
+        </div>
+        <div>
+          <label>Age:</label>
+          <input type="number" class="form-input" id="age" value="25" />
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
+
+<script >
+let firsName=document.getElementById("firstName").value;
+console.log(firsName)
+</script>
