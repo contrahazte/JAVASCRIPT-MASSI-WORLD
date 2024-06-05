d2a621e1af58e64458dedbee4dbd6efa0ea06b74
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-06-05 13:41:50 +0200

    Update nan.js

--- nan.js
+++ nan.js
@@ -1,12 +1,15 @@
-const order = {
-    "customer":{
-    "address":{
-    "city":[],}
-    }}
-    
+//In this example we are trying to check if a value for the city property is defined. However, the code can be simplified. How can we do that?
 
-   
-    
-    if(Object.values(order.customer.address.city===NaN)){
-     console.log("city is required") }
- 
+const order = {
+    customer:{
+        address:{
+            city:"Málaga"
+            }}};
+
+// if (order && order.customer && order.customer.address && !order.customer.address.city) {
+//   console.log('City is required');}
+
+  if (order.customer.address.city===undefined) {
+    console.log('City is required');
+  }
+    else{console.log(Object.values(order.customer.address))}
