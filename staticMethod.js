64317c8b65fc219d33f358120b0e14c215e20723
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-06-08 21:54:03 +0200

    Update staticMethod.js

--- staticMethod.js
+++ staticMethod.js
@@ -1,21 +1,21 @@
-class Person {
-    constructor(id, firstName, lastName, age) {
-      this.id = id;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-    }
-  
-    toJson() {
-      return JSON.stringify(this);
-    }
-    static fromJson(json) {
-      const obj = JSON.parse(json);
-      return new Person(obj.id, obj.firstName, obj.lastName, obj.age);
-    }
 
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
   }
-  
-  const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}'; 
-  const developer = Person.fromJson(json);
-  console.log(developer);
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+ static toParse(someJson){
+    return JSON.parse(someJson);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.toParse(json);
+console.log(developer);
