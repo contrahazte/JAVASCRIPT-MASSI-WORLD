24e769a4725980a9284f5a031d64b84404bf80c6
contrahazte <164871564+contrahazte@users.noreply.github.com>
2024-06-08 21:37:05 +0200

    Update adults.js

--- adults.js
+++ adults.js
@@ -1,14 +1,17 @@
+
 const person = {
-    id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age:25
-  };
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 16
+};
 
-  function isAdult(age) {
-    return person.age >= 18? age: console.log("Come back when you grow up")
-  }
 
-  const{id,firstName,lastName,age}=person
+function adultsFilter(somePeople){
+  let{age}=somePeople
 
-  console.log(isAdult(age))
+  if(age<=18){console.log("come back when you grow up")}
+  else{console.log(age)}
+}
+
+adultsFilter(person)
